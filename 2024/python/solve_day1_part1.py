1395270a3b5617a4a817103f2df13aedf41a6c3f
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-11-04 14:16:07 +0000

    Add dummy Python function

---
+++ 2024/python/solve_day1_part1.py
@@ -0,0 +1,11 @@
+def solve_day1_part1(input):
+  """
+  Dummy function to test Python content generation
+  
+  Args:
+    input (list of str): List of lines read from input.
+    
+  Returns: 
+    The answer!
+  """
+  return "Huzzah!"
