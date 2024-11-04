19f1124a4273a6c6be65be0786fdc85660fcebfb
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-11-04 10:53:15 +0000

    Add function to time execution

--- utils/aoc_utils.R
+++ utils/aoc_utils.R
@@ -7,3 +7,19 @@ aoc_source <- function(day, part) {
   path <- paste0("R/solve_day", day, "_part", part, ".R")
   source(path)
 }
+
+aoc_run <- function(expr) {
+  expr <- substitute(expr)
+
+  start_time <- proc.time()
+
+  result <- eval(expr)
+
+  time_diff <- proc.time() - start_time
+  elapsed   <- time_diff[["elapsed"]]
+
+  cat("Answer:  ", result, "\n")
+  cat("Elapsed: ", elapsed, " seconds\n")
+
+  invisible(list(result, elapsed))
+}
