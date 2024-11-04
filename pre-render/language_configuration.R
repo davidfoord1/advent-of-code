20f3fee9e404c0ee233f2594d5d36382ff1b1fb1
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-11-04 13:54:03 +0000

    Add Python to language configuration

--- pre-render/language_configuration.R
+++ pre-render/language_configuration.R
@@ -6,6 +6,14 @@ lang_configs <- list(
     extension = "R",
     code_chunk_lang = "r",
     aoc_run_template = 'aoc_run(solve_day%d_part%d(input))'
+  ),
+
+  Python = list(
+    name = "Python",
+    folder = "python",
+    extension = "py",
+    code_chunk_lang = "python",
+    aoc_run_template = 'aoc_run("solve_day%d_part%d(input)")'
   )
 
   # Add more languages here
