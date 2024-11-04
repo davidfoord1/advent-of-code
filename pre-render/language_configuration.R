defaffc21314b1d7251e96ee6ccf816974238351
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-11-04 19:17:13 +0000

    Add description to language config list

--- pre-render/language_configuration.R
+++ pre-render/language_configuration.R
@@ -1,3 +1,13 @@
+# `folder` is the subdirectory for the language to be found in each year
+# directory
+
+# `extension` is the file extension to search for
+
+# `code_chunk_lang` is Quarto code chunk supported language name
+
+# `aoc_run_template` is a base::sprintf() supported string of the current
+# required to run the solution function for a given day and part, i.e. it should
+# have 2x %d
 lang_configs <- list(
 
   R = list(
