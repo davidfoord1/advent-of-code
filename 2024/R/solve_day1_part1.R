bfa28b712cb78971420c436c6f76ff92bfb80996
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-11-04 10:49:24 +0000

    Add dummy function for first part

---
+++ 2024/R/solve_day1_part1.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param input
+#' Character vector of lines read from input.
+#'
+#' @return
+#' Answer
+solve_day1_part1 <- function(input) {
+  if (2 * 3 == 6) {
+    "testing content generation"
+  }
+}
