fe738ae94ecc05997ee52d94d413df2264c26519
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-11-04 11:03:37 +0000

    Create language configuration file

---
+++ pre-render/language_configuration.R
@@ -0,0 +1,12 @@
+lang_configs <- list(
+
+  R = list(
+    name = "R",
+    folder = "R",
+    extension = "R",
+    code_chunk_lang = "r",
+    aoc_run_template = 'aoc_run(solve_day%d_part%d(input))'
+  )
+
+  # Add more languages here
+)
