fb3405aa18d2b69a8b525371a6858f1ad5e3825d
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-11-05 21:00:55 +0000

    Center expanded  content column

---
+++ 2024/R/solve_day2_part1.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param x What if talk about this parameter really long, how does this look in
+#' the code chunk?
+#'
+#' @return
+#' @export
+#'
+#' @examples
+solve_day2_part1 <- function(x) {
+  "blah"
+}
