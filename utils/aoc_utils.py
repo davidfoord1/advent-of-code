41d6aea42006995aaa591ff8ac069589cbbaf118
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-11-08 20:02:18 +0000

    Add memory measurement to Python utils

--- utils/aoc_utils.py
+++ utils/aoc_utils.py
@@ -1,19 +1,38 @@
 import time
+import tracemalloc
   
 def aoc_run(expr):
+    tracemalloc.start()
+    
     start_time = time.time()
     
+    # Run the function
     result = eval(expr)
     
-    elapsed = round(time.time() - start_time, 2)
+    end_time = time.time()
     
-    if elapsed < 0.01:
-      elapsed = "< 0.01"
+    current, peak = tracemalloc.get_traced_memory()
     
-    print("Answer: ", result)
+    tracemalloc.stop()
+    
+    elapsed = end_time - start_time
+    
+    if elapsed < 0.001:
+      elapsed = "< 0.001"
+    else:
+      elapsed = round(elapsed, 3)
+      
+    mem_used = peak / 1024
+    
+    if mem_used < 1:
+      mem_used = "< 1"
+    else:
+      mem_used = round(mem_used)
+    
     print("Elapsed:", elapsed, "seconds")
+    print("Memory: ", mem_used, "KB")
     
-    return result, elapsed
+    return elapsed, mem_used
 
 def aoc_read(day):
   day = str(day)
