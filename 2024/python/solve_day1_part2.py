c06a859ff650db03b862a653501e2e8a4654d834
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-01 08:46:48 +0000

    Add 2024 day 1 part 2 py

---
+++ 2024/python/solve_day1_part2.py
@@ -0,0 +1,36 @@
+def solve_day1_part2(input):
+  """
+  Score the similarities between 2 lists
+  
+  Parse the input text as 2 lists of numbers. Count the number of occurrences 
+  of each item in the first list in the second list. Multiply each number by
+  its count to get the similarity score. Finally sum the score.
+
+  The list count method made this one fairly easy. Again I imagine there is a
+  slightly slicker way then using a for loop, but not bad to have the basics!
+  
+  Parameters
+  ----------
+  input : list of str
+      Where each string contains a number in the first list and a the second 
+      list.
+
+  Returns
+  -------
+  int
+      The sum of absolute differences between the sorted lists
+  """
+    
+  # extract to 2 lists of integers for ease of operations
+  lines = [line.split() for line in input]
+  first_list = [int(line[0]) for line in lines]
+  second_list = [int(line[1]) for line in lines]
+  
+  counts = [second_list.count(num) for num in first_list]
+  
+  similarity_scores = [None] * len(first_list)
+  
+  for i in range(len(first_list)):
+    similarity_scores[i] = first_list[i] * counts[i]
+  
+  return(sum(similarity_scores))
