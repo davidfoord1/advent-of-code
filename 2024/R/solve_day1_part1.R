ecde762f9ae1edef523e5382e7c487e1f6e1cf53
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-01 06:48:26 +0000

    Add 2024 day 1 part 1

--- 2024/R/solve_day1_part1.R
+++ 2024/R/solve_day1_part1.R
@@ -1,12 +1,13 @@
-#' Title
-#'
-#' @param input
-#' Character vector of lines read from input.
-#'
-#' @return
-#' Answer
 solve_day1_part1 <- function(input) {
-  if (2 * 3 == 6) {
-    "testing content generation"
-  }
+  lists <- strsplit(input, " +")
+
+  first_list <- lapply(lists, \(x) x[[1]])
+  first_list <- sort(as.numeric(first_list))
+
+  second_list <- lapply(lists, \(x) x[[2]])
+  second_list <- sort(as.numeric(second_list))
+
+  distances <- abs(second_list - first_list)
+
+  sum(distances)
 }
