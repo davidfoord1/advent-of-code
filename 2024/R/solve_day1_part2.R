de289ab5add556e292c61db2e7bdf9bcdcf7e7b9
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-01 07:02:22 +0000

    Add 2024 R day 1 part 2

--- 2024/R/solve_day1_part2.R
+++ 2024/R/solve_day1_part2.R
@@ -1,5 +1,16 @@
 solve_day1_part2 <- function(input) {
-  # This function will take slightly longer
+  lists <- strsplit(input, " +")
 
-  sum(runif(1e8))
+  first_list <- lapply(lists, \(x) x[[1]])
+  first_list <- sort(as.numeric(first_list))
+
+  second_list <- lapply(lists, \(x) x[[2]])
+  second_list <- sort(as.numeric(second_list))
+
+  counts <- lapply(first_list, \(num) sum(second_list == num))
+  counts <- as.numeric(counts)
+
+  similarity_scores <- first_list * counts
+
+  sum(similarity_scores)
 }
