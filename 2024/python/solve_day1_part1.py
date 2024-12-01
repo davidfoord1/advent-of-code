fd2549f4843c8face29615564ddf51edf854af25
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-01 08:08:54 +0000

    Add 2024 day 1 part 1 py docstring

--- 2024/python/solve_day1_part1.py
+++ 2024/python/solve_day1_part1.py
@@ -1,4 +1,27 @@
 def solve_day1_part1(input):
+  """
+  Compare the differences between 2 lists
+  
+  Parse the input text as 2 lists of numbers and sort 
+  them. Then take the sum of the absolute differences between pairs.
+  
+  I imagine it's not too 'pythonic' even for such a simple puzzle. In porting
+  my R solution with the limited Python that I know, lapply has become list 
+  comprehensions and I've used a for loop to iterate over the 2 lists instead
+  of a vectorised comparison. Perhaps NumPy could help out there?
+
+  Parameters
+  ----------
+  input : list of str
+      Where each string contains a number in the first list and a the second 
+      list.
+
+  Returns
+  -------
+  int
+      The sum of absolute differences between the sorted lists
+  """
+    
   # extract to 2 lists of integers for ease of operations
   lines = [line.split() for line in input]
   first_list = [int(line[0]) for line in lines]
