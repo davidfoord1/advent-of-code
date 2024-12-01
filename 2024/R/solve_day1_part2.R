ba89888e0f3f5cfb0a73eb608bac9a14883d905f
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-01 07:29:09 +0000

    Add 2024 R day 1 part 2 docstring

--- 2024/R/solve_day1_part2.R
+++ 2024/R/solve_day1_part2.R
@@ -1,14 +1,31 @@
+#' Score the similarities between 2 lists
+#'
+#' Parse the input text as 2 lists of numbers. Count the number of occurrences
+#' of each item in the first list in the second list. Multiply each number by
+#' its count to get the similarity score. Finally sum the score.
+#'
+#' To count I used the sum of equality between each number in the second
+#' list and the number being counted from the first list.
+#'
+#' @param input
+#' A character vector with 2 numbers per string
+#'
+#' @return
+#' The sum of absolute differences between the sorted lists
 solve_day1_part2 <- function(input) {
+  # extract to 2 vectors for ease of operations
   lists <- strsplit(input, " +")
-
   first_list <- lapply(lists, \(x) x[[1]])
-  first_list <- sort(as.numeric(first_list))
-
   second_list <- lapply(lists, \(x) x[[2]])
-  second_list <- sort(as.numeric(second_list))
 
-  counts <- lapply(first_list, \(num) sum(second_list == num))
-  counts <- as.numeric(counts)
+  first_list <- as.numeric(first_list)
+  second_list <- as.numeric(second_list)
+
+  # This time we need to count how many times a number in the first list
+  # occurs in the second list. We can do that as the sum of logical equality
+  # between each number in the second list and the number being counted from
+  # the first list.
+  counts <- vapply(first_list, \(num) sum(second_list == num), numeric(1))
 
   similarity_scores <- first_list * counts
 
