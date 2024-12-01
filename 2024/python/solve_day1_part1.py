117699a0f83384581fe9cccbb1b544dc77de7956
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-01 07:55:43 +0000

    Add 2024 day 1 part 1 py

--- 2024/python/solve_day1_part1.py
+++ 2024/python/solve_day1_part1.py
@@ -1,11 +1,21 @@
 def solve_day1_part1(input):
-  """
-  Dummy function to test Python content generation
+  # extract to 2 lists of integers for ease of operations
+  lines = [line.split() for line in input]
+  first_list = [int(line[0]) for line in lines]
+  second_list = [int(line[1]) for line in lines]
   
-  Args:
-    input (list of str): List of lines read from input.
-    
-  Returns: 
-    The answer!
-  """
-  return "Huzzah!"
+  # sort so that we pair up the smallest number in the left list with the
+  # smallest number in the right list etc.
+  first_list.sort()
+  second_list.sort()
+  
+  distances = [None] * len(first_list)
+  
+  # we want the absolute distance
+  # it doesn't matter which list has the larger number
+  for i in range(len(first_list)):
+    distances[i] = abs(second_list[i] - first_list[i])
+  
+  return(sum(distances))
+  
+  
