63fb768f84cc3208bb7a1fe787fdfcff60b3481b
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-02 10:50:43 +0000

    Add 2024 day 2 part 1 R

--- 2024/R/solve_day2_part1.R
+++ 2024/R/solve_day2_part1.R
@@ -1,12 +1,17 @@
-#' Title
-#'
-#' @param x What if talk about this parameter really long, how does this look in
-#' the code chunk?
-#'
-#' @return
-#' @export
-#'
-#' @examples
-solve_day2_part1 <- function(x) {
-  "blah"
+solve_day2_part1 <- function(input) {
+  num_list <- strsplit(input, " ")
+  num_list <- lapply(num_list, as.numeric)
+
+  diff_list <- lapply(num_list, diff)
+
+  safe_count <- 0
+
+  for (diffs in diff_list) {
+    diffs <- unlist(diffs)
+    if ((all(diffs < 0) | all(diffs > 0)) & all(abs(diffs) %in% c(1, 2, 3))) {
+      safe_count <- safe_count + 1
+    }
+  }
+
+  safe_count
 }
