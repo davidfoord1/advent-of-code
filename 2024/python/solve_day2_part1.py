70b488c242ea2487380e0bdc45262b663e613341
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-02 18:35:53 +0000

    Add 2024 day 2 part 1 py docstrings

--- 2024/python/solve_day2_part1.py
+++ 2024/python/solve_day2_part1.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 def solve_day2_part1(input):
+  """
+  Find how many reports are safe
+  
+  Check each report is
+     either all decreasing OR all increasing
+     AND all absolute differences are within 1 to 3
+  
+    Parameters
+  ----------
+  input : list of str
+      Where each string is a report and each digit is a 'level'
+
+  Returns
+  -------
+  int
+    The number of safe reports
+  
+  """
   reports_list = [line.split() for line in input]
   reports_list = [np.array(report, dtype = np.int32) for report in reports_list]
     
@@ -9,6 +27,23 @@ def solve_day2_part1(input):
   return(sum([is_safe(diffs) for diffs in diffs_list]))
   
 def is_safe(diffs):
+  """
+  Check if the differences are safe
+  
+  Check the differences for
+     either all decreasing OR all increasing
+     AND all absolute differences are within 1 to 3
+  
+  Parameters
+  ----------
+  diffs : numpy array of int
+      Where each int is a difference between consecutive levels in a report
+
+  Returns
+  -------
+  bool
+      Whether the differences are safe
+  """
   one_direction = all(diffs < 0) or all(diffs > 0)
   safe_size = all(abs(diffs) <= 3)
   
