5f08da18e162d0acd5a0698e3e7687b1badb17f9
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-02 11:40:27 +0000

    Add 2024 day 2 part 2 R docstrings

--- 2024/R/solve_day2_part2.R
+++ 2024/R/solve_day2_part2.R
@@ -1,3 +1,26 @@
+#' Find how many reports are safe with dampeners
+#'
+#' Check each report is
+#'   either all decreasing OR all increasing
+#'   AND all absolute differences are within 1 to 3
+#'
+#' As well as whether the same can be achieved by removing one of the levels.
+#'
+#' I took what seems to be a long-winded approach of trying to identify which
+#' levels caused unsafe differences. Then I tried reports with those levels
+#' removed and tested if they were safe. My first attempt included checking for
+#' a majority direction of differences and replacing the differences in the
+#' minority, but I forgot to deal with no majority!
+#'
+#' I see from others that it would be simpler to just try removing each and
+#' every level, which is plenty feasible with the input.
+#'
+#' @param input
+#' A character vector, where each represents  a report, containing space
+#' delimited sequence of digits, 'levels'
+#'
+#' @return
+#' numeric(1) The number of safe reports.
 solve_day2_part2 <- function(input) {
   num_list <- strsplit(input, " ")
   num_list <- lapply(num_list, as.numeric)
@@ -7,41 +30,80 @@ solve_day2_part2 <- function(input) {
   sum(safe_list)
 }
 
+#' Check if a report is safe
+#'
+#' Find the majority direction in differences (increasing/decreasing) if there
+#' is one. Mark the minority direction as unsafe diffs. Mark absolute diffs
+#' that aren't in 1, 2, 3 as unsafe. For each unsafe diff check if the report
+#' would be safe by removing the diff either side of it.
+#'
+#' If there is no majority direction then the report is unsafe because the
+#' dampener won't be enough. (I suppose this assumes report length is not 3,
+#' which could be made safe)
+#'
+#' @param report
+#' Numeric levels to be checked
+#'
+#' @return
+#' logical(1) whether the report is safe
 is_safe <- function(report, removed = 0) {
   diffs <- diff(report)
+  majority_diffs <- length(diffs) / 2 + 1
 
-  if (removed == 1) {
-    one_direction <- all(diffs < 0) | all(diffs > 0)
-    safe_amount <- all(abs(diffs) <= 3)
-    return(one_direction & safe_amount)
-  }
-
-  if (max(sum(diffs < 0), sum(diffs > 0)) < 3) {
+  # no majority direction
+  # fails all increasing OR all decreasing rule
+  # dampener won't be enough
+  if (max(sum(diffs < 0), sum(diffs > 0)) < majority_diffs) {
     return(FALSE)
   }
 
+  # logical to store location of unsafe diffs
   unsafe_diffs <- logical(length(diffs))
 
+  # if majority decreasing, unsafe where increasing
   if (sum(diffs < 0) >= 3) {
-    unsafe_diffs <- diffs >= 0
+    unsafe_diffs <- diffs > 0
   }
 
+  # if majority increasing, unsafe where decreasing
   if (sum(diffs > 0) >= 3) {
-    unsafe_diffs <- unsafe_diffs | diffs <= 0
+    unsafe_diffs <- unsafe_diffs | diffs < 0
   }
 
-  unsafe_diffs <- unsafe_diffs | diffs == 0
-  unsafe_diffs <- unsafe_diffs | !abs(diffs) <= 3
+  # unsafe where difference not in safe size range
+  unsafe_diffs <- unsafe_diffs | !abs(diffs) %in% c(1, 2, 3)
 
   if (!any(unsafe_diffs)) {
     # no unsafe levels
     TRUE
   } else {
-    # try removing each level either side of the unsafe diff
+    # create new reports by removing each level either side of the unsafe diffs
     unsafe_levels <- which(unsafe_diffs)
     unsafe_levels <- c(unsafe_levels, unsafe_levels + 1)
     new_reports <- lapply(unsafe_levels, \(level) report[-level])
 
-    any(vapply(new_reports, is_safe, logical(1), removed = 1))
+    # check if the shortened report is safe without further option
+    # to remove levels
+    any(vapply(new_reports, is_safe_no_dampener, logical(1)))
   }
+}
+
+#' Check if the rest of a report is safe
+#'
+#' Check the differences for
+#'   either all less than zero OR all greater than zero
+#'   AND all absolute differences are within 1 to 3
+#'
+#' @param report
+#' Numeric levels to be checked
+#'
+#' @return
+#' logical(1) whether the report is safe
+is_safe_no_dampener <- function(report) {
+  diffs <- diff(report)
+
+  one_direction <- all(diffs < 0) | all(diffs > 0)
+  safe_size <- all(abs(diffs) %in% c(1, 2, 3))
+
+  one_direction & safe_size
 }
