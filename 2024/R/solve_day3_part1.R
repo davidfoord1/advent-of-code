102e0f24786a5f1f1e6561646ff7ee19a32cd11a
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-03 07:35:13 +0000

    Update 2024 day 3 part 1 R

--- 2024/R/solve_day3_part1.R
+++ 2024/R/solve_day3_part1.R
@@ -1,10 +1,21 @@
+#' Sum of multiplications
+#'
+#' Uses regex to find numbers in format mul(x, y). Multiplies each pair and
+#' adds the results together.
+#'
+#' @param input
+#' Character vector of lines to parse for numbers to multiply.
+#'
+#' @return
+#' The sum of results of multiplications
 solve_day3_part1 <- function(input) {
   muls <- stringi::stri_extract_all_regex(input,
                                           "(?<=mul\\()[0-9]+,[0-9]+(?=\\))")
 
   muls <- unlist(muls)
   muls <- strsplit(muls, ",")
-  muls <- as.data.frame(t(as.data.frame(muls)))
+  muls <- purrr::list_transpose(muls)
+  muls <- lapply(muls, as.numeric)
 
-  sum( as.numeric(muls[ ,1]) * as.numeric(muls[, 2]) )
+  sum(muls[[1]] * muls[[2]])
 }
