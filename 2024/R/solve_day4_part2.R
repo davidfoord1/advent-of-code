7e28acaaf63c03300bd6bae099f2e0e415b642a6
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-05 08:58:57 +0000

    Split 2024 day 4 R to part 2

---
+++ 2024/R/solve_day4_part2.R
@@ -0,0 +1,64 @@
+#' Complete an XMAS word search
+#'
+#' Iterate over the word search as a matrix. When you find an "X", check
+#' around it for the full word "XMAS",
+#'
+#' @param input
+#' A character vector of lines as part of a word search grid
+#'
+#' @return
+#' The number of occurrences of "XMAS" in the word search
+solve_day4_part2 <- function(input) {
+  # get input into a matrix
+  # this clunky line is because I've stuck to readLines for the input
+  grid <- t(matrix(unlist(strsplit(input, "")), nrow = length(input)))
+
+  # add padding to avoid out of bounds checks
+  empty_row <- rep(".", NCOL(grid))
+  grid <- rbind(empty_row, grid, empty_row)
+
+  empty_col <- rep(".", NROW(grid))
+  grid <- cbind(empty_col, grid, empty_col)
+
+  x_mas_count <- 0
+
+  # go through each positino in the grid
+  for (row in seq_len(NROW(grid))) {
+    for (col in seq_len(NCOL(grid))) {
+
+      # counting the number of "MAS"s in a cross formation around an "A"
+      if (grid[row, col] == "A") {
+        x_mas_count <- x_mas_count + find_x_mas(grid, row, col)
+      }
+
+    }
+  }
+
+  x_mas_count
+}
+
+#' Search diagonals from an "A" for "M" and "S"
+#'
+#' In 2 diagonals, check the adjacent positions contain "M" and "S". Sort
+#' each diagonal so it doesn't matter which way around the "M" and "S" are.
+#'
+#' @param grid
+#' Character matrix to search.
+#' @param row
+#' Row position of "A" to search from.
+#' @param col
+#' Column position of "A" to search from.
+#'
+#' @return
+#' numeric(1) 1 when a cross of "MAS" was found at the given location,
+#' otherwise 0.
+find_x_mas <- function(grid, row, col) {
+  words <- list()
+  # up left  down right
+  words[[1]] <- sort(c(grid[row - 1, col - 1], grid[row + 1, col + 1]))
+  # up-right down-left
+  words[[2]] <- sort(c(grid[row - 1, col + 1], grid[row + 1, col - 1]))
+
+  sum(vapply(words, \(word) identical(word, c("M", "S")), logical(1))) == 2
+}
+
