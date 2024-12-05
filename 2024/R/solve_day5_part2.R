750d1b2885c153195af9dbd72e67aa1c7173ef88
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-05 08:15:17 +0000

    Update 2024 day 5 part 2 R

--- 2024/R/solve_day5_part2.R
+++ 2024/R/solve_day5_part2.R
@@ -1,42 +1,80 @@
+#' Find the middle number of unsorted lists
+#'
+#' Given a sort of page ordering pairs x|y where y is after x. Store each y
+#' against the same x in an environment (as a hashmap).
+#'
+#' Given lists of page updates, check each page for breaching the ordering rules
+#' stored in the hashmap. For any updates that *do* breach, we find its middle
+#' page number and sum them.
+#'
+#' @param input
+#' Character vector of lines read from input. It has two sections - page
+#' ordering pairs and lists of page numbers.
+#'
+#' @return
+#' The sum of middle numbers of unsorted page update lists.
 solve_day5_part1 <- function(input) {
   sep <- which(nchar(input) == 0)
 
+  # ordering rules map ----
   ordering <- input[1:(sep-1)]
   ordering <- strsplit(ordering, "\\|")
 
+  # using an environment as a hash map
   after_map <- new.env()
 
+  # for each x|y pair
+  # store y in the map against x as the key
   for (i in seq_along(ordering)) {
-    key <- ordering[[i]][[1]]
-    value <- ordering[[i]][[2]]
-    if (is.null(after_map[[key]])) {
-      after_map[[key]] <- value
-    } else {
-      after_map[[key]] <- sort(c(after_map[[key]], value))
-    }
+    pair <- ordering[[i]]
+    key <- pair[[1]]
+    value <- pair[[2]]
+
+    # we don't need to sort here, but I did for some quick visual scanning
+    after_map[[key]] <- sort(c(after_map[[key]], value))
   }
 
+  # pages to produce in each update ----
   updates <- input[(sep+1):length(input)]
   updates <- strsplit(updates, ",")
 
+
+  # check and extract ----
+  # storage for middle page numbers
   middle_nums <- numeric(length(updates))
 
+  # iterate to find lists which aren't sorted
   for (i in seq_along(updates)) {
     update <- updates[[i]]
 
-    if (!is_ordered(updates[[i]], after_map)) {
+    if (!is_sorted(updates[[i]], after_map)) {
+      # then sort them and store their middle page number
       update <- sort_update(update, after_map)
 
 
       middle_index <- ceiling(length(update) / 2)
-      middle_nums[[i]] <- update[[middle_index]]
+      middle_nums[[i]] <- as.numeric(update[[middle_index]])
     }
   }
 
-  sum(as.numeric(middle_nums))
+  sum(middle_nums)
 }
 
-is_ordered  <- function(update, after_map) {
+#' Whether a sequence of pages is in order
+#'
+#' Working backwards from the last page in the update to the first. Check
+#' the pages before it in the list against that pages after_map list. If
+#' any page is before when it should be after, the list is not sorted
+#'
+#' @param update
+#' Character vector of page numbers
+#' @param after_map
+#' Environment, key-value pairs where each value is a character vector of pages
+#' that should come after the key
+#'
+#' @return
+#' Logical whether the pages are in order
+is_sorted  <- function(update, after_map) {
   for (i in seq(length(update), 1, -1)) {
     check_num <- update[[i]]
     to_check <- update[i:1]
@@ -49,10 +87,26 @@ is_ordered  <- function(update, after_map) {
   TRUE
 }
 
+#' Sort a list of pages
+#'
+#' An implementation of bubble sort. We iterate along the list of pages in
+#' pairs. If the first number in the pair is in the after list of the second
+#' number in the pair, we swap the top numbers. Repeat from the start until all
+#' numbers have been sorted.
+#'
+#' @param update
+#' A character vector of page numbers to sort
+#' @param after_map
+#' #' Environment, key-value pairs where each value is a character vector of
+#' pages that should come after the key
+#'
+#' @return
+#' A sorted version of `update`
 sort_update <- function(update, after_map) {
-  sorted <- character()
+  # this is whether the number has been sorted, not the resulting sorted list
+  been_sorted <- character()
 
-  while (length(sorted) != length(update)) {
+  while (length(been_sorted) != length(update)) {
     for (i in seq_along(update)) {
       check_num <- update[[i]]
       to_check <- update[-i]
@@ -63,7 +117,7 @@ sort_update <- function(update, after_map) {
         update <- c(to_check[-ooo], check_num, to_check[ooo])
       }
 
-      sorted <- unique(c(sorted, check_num))
+      been_sorted <- unique(c(been_sorted, check_num))
     }
   }
 
