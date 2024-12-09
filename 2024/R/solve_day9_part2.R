a6e3912782a05ec200ab0b47b61f521758a1f5f4
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-12-09 21:26:27 +0000

    Rework 2024 day 9 part 2 R

--- 2024/R/solve_day9_part2.R
+++ 2024/R/solve_day9_part2.R
@@ -1,96 +1,88 @@
-solve_day9_part2 <- function (input) {
-  lengths <- as.integer(unlist(strsplit(input, "")))
+#' Find the filesystem checksum
+#'
+#' Convert given lengths list alternating between files with ascending ID and
+#' spaces - the full disk layout. Find the length of continous sections with
+#' `rle`. Iterate down the file IDs, searching earlier on the disk (in the list)
+#' to find a space of sufficient length to move the file to.
+#'
+#' Multiply each instance of the ID by its new position and sum the result.
+#'
+#' @param input
+#' Single string of digits where they alternate between indicating the length of
+#' a file and the length of free space.
+#'
+#' @return
+#' numeric(1) Filesystem checksum
+solve_day9_part2 <- function(input) {
+  lens <- as.integer(unlist(strsplit(input, "")))
+  lens_seq <- seq_along(lens)
+  lens_len <- length(lens)
 
-  id <- 0L
-  is_file <- TRUE
-  disk <- vector("list", length(lengths))
+  # alternating lengths are files
+  is_file <- lens_seq %% 2L == 1L
 
-  for (i in seq_along(lengths)) {
-    if (is_file) {
-      disk[[i]] <- list(rep(id, lengths[[i]]))
+  # ids start from 0 and increase every 2 lengths
+  ids <- (lens_seq - 1L) * is_file / 2L
+  ids <- lapply(lens_seq, \(i) rep(ids[[i]], lens[[i]]))
 
-      id <- id + 1L
-      is_file <- FALSE
-    } else {
-      disk[[i]] <- list(rep(".", lengths[[i]]))
+  spaces <- rep(".", lens_len)
+  spaces <- lapply(lens_seq, \(i) rep(spaces[[i]], lens[[i]]))
 
-      is_file <- TRUE
+  disk <- ifelse(is_file, ids, spaces)
+  disk <- unlist(disk, recursive = FALSE)
+
+  space_rle <- rle(disk == ".")
+
+  disk_rle <- rle(disk)
+
+  # we take the lengths of the non-space values + 1
+  # to get the start of the next space block
+  space_starts <- cumsum(disk_rle[["lengths"]]) - lag(disk_rle[["lengths"]]) + 1L
+
+  spaces <- data.frame(
+    starts = space_starts[disk_rle[["values"]] == "."],
+    lens = disk_rle[["lengths"]][disk_rle[["values"]] == "."]
+  )
+
+  file_ends <- cumsum(disk_rle[["lengths"]])
+  file_ends <- file_ends[!disk_rle[["values"]] == "."]
+  file_lens <- disk_rle[["lengths"]][!disk_rle[["values"]] == "."]
+  file_vals <- disk_rle[["values"]][!disk_rle[["values"]] == "."]
+
+  for (file in rev(seq_along(file_ends))) {
+    file_end <- file_ends[[file]]
+    file_len <- file_lens[[file]]
+
+    # find space before the block and of sufficient length
+    valid_spaces <- which(spaces[["starts"]] <= file_end &
+                           spaces[["lens"]] >= file_len)
+
+    if (length(valid_spaces) > 0) {
+      valid_space <- valid_spaces[[1L]]
+
+      space_start <- spaces[["starts"]][valid_space]
+      space_len <-  spaces[["lens"]][valid_space]
+
+      # move file to space
+      disk[space_start:(space_start + file_len - 1L)] <- file_vals[[file]]
+
+      # store remaining space
+      new_len <- space_len - file_lens[[file]]
+      spaces[["lens"]][valid_space] <- new_len
+
+      if (new_len > 0L) {
+        spaces[["starts"]][valid_space] <-
+          spaces[["starts"]][valid_space] + file_len
+      }
+
+      # write space to old file location
+      disk[file_end:(file_end - file_len + 1L)] <- "."
     }
+
   }
 
-  cont <- unlist(disk)
-
-  first_space <- 1L
-  first_space <- next_space(cont, first_space)
-  block_ptr <- length(cont)
-
-  # ----
-
-  while(first_space < block_ptr) {
-    if (cont[[block_ptr]] == ".") {
-      block_ptr <- block_ptr - 1L
-      next
-    }
-
-    block_id <- cont[[block_ptr]]
-    block_len <- 1L
-
-    for (i in (block_ptr-1L):1L) {
-      if (cont[[i]] != block_id) {
-        break
-      }
-
-      block_len <- block_len + 1L
-    }
-
-    space_ptr <- first_space
-    repeat {
-      if (cont[[space_ptr]] != ".") {
-        space_ptr <- space_ptr + 1L
-        next
-      }
-
-      space_len <- 1L
-
-      for (i in (space_ptr+1L):block_ptr) {
-        if (cont[[i]] != ".") {
-          break
-        }
-
-        space_len <- space_len + 1L
-      }
-
-      if (space_len >= block_len) {
-        space_pos <- space_ptr:(space_ptr + block_len - 1L)
-        block_pos <- block_ptr:(block_ptr - block_len + 1L)
-
-        cont[space_pos] <- cont[block_pos]
-        cont[block_pos] <- "."
-
-        block_ptr <- block_ptr - block_len
-        first_space <- next_space(cont, first_space)
-        break
-      } else {
-        space_ptr <- space_ptr + 1L
-        space_ptr <- next_space(cont, space_ptr)
-      }
-
-      if (space_ptr >= block_ptr) {
-        block_ptr <- block_ptr - block_len
-        break
-      }
-    }
-  }
-
-  cont[cont == "."] <-  0L
-  cont <- as.numeric(cont)
-  sum(cont * 0L:(length(cont) - 1L), na.rm = TRUE)
+  disk[disk == "."] <- 0L
+  disk <- as.numeric(disk)
+  sum(disk * 0L:(length(disk) - 1L))
 }
 
-next_space <- function(disk, ptr) {
-  for (i in ptr:length(disk)) {
-    if (disk[[i]] == ".") {
-      return(i)
-    }
-  }
-}
