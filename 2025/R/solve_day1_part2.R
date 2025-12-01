65c1d918779caa67b8c5dabcfe820f04d8be9c97
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-01 20:32:46 +0000

    Add div solution 2025 day 1 part 2 R

--- 2025/R/solve_day1_part2.R
+++ 2025/R/solve_day1_part2.R
@@ -1,32 +1,29 @@
 solve_day1_part2 <- function(input) {
   pos <- 50L
   dial_size <- 100L
-  zero_counter <- 0L
+  counter <- 0L
 
   dir <- substr(input, 1L, 1L)
   val <- as.integer(substr(input, 2L, nchar(input)))
   turns <- ifelse(dir == "R", val, -val)
 
   for (turn in turns) {
-    for (k in seq_len(abs(turn))) {
-      if (turn < 0) {
-        dir <- -1L
-      } else {
-        dir <- 1
-      }
+    # position on infinite number line
+    abs_pos <- pos + turn
 
-      pos <- pos + dir
-
-      if (pos == -1) {
-        pos <- 99
-      }
-      if (pos == 100) {
-        pos <- 0
-      }
-
-      zero_counter <- zero_counter + (pos == 0)
+    # count multiple of 100 between the start and end
+    if (turn > 0) {
+      counter <- counter +
+        (abs_pos %/% dial_size) -
+        (pos %/% dial_size)
+    } else {
+      counter <- counter +
+        (pos - 1L) %/% dial_size -
+        (abs_pos - 1L) %/% dial_size
     }
+
+    pos <- (pos + turn) %% dial_size
   }
 
-  zero_counter
+  counter
 }
