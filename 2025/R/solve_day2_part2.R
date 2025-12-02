fa479c14935b0b3d01871ff1c4ae0fb96cbcf8c2
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-02 19:42:30 +0000

    Change to regex

--- 2025/R/solve_day2_part2.R
+++ 2025/R/solve_day2_part2.R
@@ -1,5 +1,4 @@
 solve_day2_part2 <- function(input) {
-  # back to one line
   ranges <- paste0(input, collapse = "")
   ranges <- strsplit(ranges, ",", fixed = TRUE) |>
     unlist()
@@ -8,47 +7,10 @@ solve_day2_part2 <- function(input) {
 
   invalid_total <- 0
 
-  for (range in ranges) {
-    ids <- range[[1]]:range[[2]]
+  ids <- lapply(ranges, \(x) x[[1L]]:x[[2L]]) |>
+    unlist()
 
-    invalid_ids <- vapply(ids, is_invalid, logical(1))
+  is_invalid <- stringi::stri_detect(ids, regex = "^(\\d+)\\1+$")
 
-    invalid_total <- invalid_total + sum(ids[invalid_ids])
-  }
-
-  invalid_total
-}
-
-is_invalid <- function(id) {
-  nchars <- nchar(id)
-
-  for (i in seq_len(nchars - 1)) {
-    for (j in i:nchars) {
-      substr_len <- j - i + 1
-
-      if ((nchars - substr_len) < substr_len) {
-        break
-      }
-
-      if (nchars %% substr_len != 0) {
-        next
-      }
-
-      substr_chars <- substr(id, i, j)
-      check_count <- (nchars - substr_len) / substr_len
-
-      checks <- vapply(
-        seq_len(check_count),
-        \(multi) {
-          step <- multi * substr_len
-          substr(id, i + step, j + step) == substr_chars
-        },
-        logical(1)
-      )
-
-      if (all(checks)) return(TRUE)
-    }
-  }
-
-  FALSE
+  sum(ids[is_invalid])
 }
