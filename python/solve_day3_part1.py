06bcf591def3ef7837a7f4b5e0699ddf9de94306
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-03 17:42:31 +0000

    Add 2025 day 3 part 1 python

---
+++ python/solve_day3_part1.py
@@ -0,0 +1,23 @@
+def solve_day3_part1(text):
+    banks = [list(line.strip("\n")) for line in text]
+    joltage = [max_jolt(bank) for bank in banks]
+
+    return(sum(joltage))
+
+def max_jolt(bank, n = 2):
+    digits = [None] * n
+    prev_loc = -1
+    bank_len = len(bank)
+
+    for i in range(n):
+        max_loc = bank_len - (n - i) + 1
+
+        search_bank = bank[(prev_loc + 1):max_loc]
+        digits[i] = max(search_bank)
+
+        loc = search_bank.index(digits[i])
+        prev_loc = prev_loc + loc + 1
+
+    result = int(''.join(digits))
+
+    return(result)
