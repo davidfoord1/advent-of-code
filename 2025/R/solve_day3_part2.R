ebeff0f4ddccdfd072e2acb7374f905432fc6f60
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-03 08:35:29 +0000

    Less hard coded numbers

--- 2025/R/solve_day3_part2.R
+++ 2025/R/solve_day3_part2.R
@@ -2,19 +2,21 @@ solve_day3_part2 <- function(input) {
   input |>
     strsplit("") |>
     lapply(as.integer) |>
-    vapply(max_jolt_12, numeric(1)) |>
+    vapply(max_jolt, numeric(1)) |>
     sum()
 }
 
-max_jolt_12 <- function(bank) {
-  digit_nums <- 1:12
-  digits <- character(12)
+#' @params bank Character vector of digits
+#' @params digit_count Numeric number of digits to use for joltage
+max_jolt <- function(bank, digit_count = 12) {
+  digit_nums <- seq_len(digit_count)
+  digits <- character(digit_count)
   prev_loc <- 0L
   len <- length(bank)
 
   for (i in digit_nums) {
-    max_loc <- len - (12 - i)
-    valid_bank <- bank[(prev_loc + 1):max_loc]
+    max_loc <- len - (digit_count - i)
+    valid_bank <- bank[(prev_loc + 1L):max_loc]
     loc <- which.max(valid_bank)
     prev_loc <- prev_loc + loc
     digits[i] <- valid_bank[loc]
