85477c0f6f9c09c14f98217ec62e5e9f975995e0
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-05 17:48:34 +0000

    Tidying to R solutions

--- 2025/R/solve_day5_part2.R
+++ 2025/R/solve_day5_part2.R
@@ -1,7 +1,7 @@
 solve_day5_part2 <- function(input) {
   split_at <- which(input == "")
-
   ranges <- input[1:(split_at - 1)]
+
   ranges <- read.table(
     text = ranges,
     sep  = "-",
@@ -16,27 +16,25 @@ solve_day5_part2 <- function(input) {
   prev_row <- 1L
 
   for (row in rows_to_check) {
-    extends <- extends_prev(ranges[row, ], new_ranges[prev_row, ])
-    if (extends) {
+    check_row <- ranges[row, ]
+    stored_row <- new_ranges[prev_row, ]
+
+    extends_prev <- check_row[["start"]] <= stored_row[["end"]] &&
+                check_row["end"] >= stored_row["end"]
+
+    if (extends_prev) {
       new_ranges[prev_row, "end"] <- ranges[row, "end"]
+      next
     }
 
-    within <- within_prev(ranges[row, ], new_ranges[prev_row, ])
-    if (!within) {
-      new_ranges[prev_row + 1L, ] <- ranges[row, ]
+    within_prev <- check_row[["start"]] >= stored_row[["start"]] &&
+      check_row[["end"]] <= stored_row[["end"]]
+
+    if (!within_prev) {
+      new_ranges[prev_row + 1L, ] <- check_row
       prev_row <- prev_row + 1L
     }
   }
 
   sum(new_ranges[["end"]] - new_ranges[["start"]] + 1L, na.rm = TRUE)
-}
-
-extends_prev <- function(check_row, stored_row) {
-  check_row[["start"]] <= stored_row[["end"]] &&
-    check_row["end"] >= stored_row["end"]
-}
-
-within_prev <- function(check_row, stored_row) {
-  check_row[["start"]] >= stored_row[["start"]] &&
-    check_row[["end"]] <= stored_row[["end"]]
 }
