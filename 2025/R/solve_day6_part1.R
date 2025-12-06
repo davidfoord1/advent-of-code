87c6b5f363c3a7f0ba774c984677d2797d634295
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-06 17:01:50 +0000

    A few more tweaks

--- 2025/R/solve_day6_part1.R
+++ 2025/R/solve_day6_part1.R
@@ -2,13 +2,12 @@ solve_day6_part1 <- function(input) {
     worksheet = read.table(text = input)
 
     nums <- worksheet[1:(NROW(worksheet)-1), ]
-    nums <- apply(nums,2, as.numeric, simplify = FALSE)
+    nums <- apply(nums, 2, as.numeric, simplify = FALSE)
 
     ops <- worksheet[NROW(worksheet), ]
     ops <- ifelse(ops == "+", list(sum), list(prod))
 
-    ans <- Map(\(fn, vec) fn(vec), ops, nums)
-    ans <- unlist(ans)
+    ans <- mapply(\(fn, vec) fn(vec), ops, nums)
 
     sum(ans)
 }
--- 2025/R/solve_day6_part2.R
+++ 2025/R/solve_day6_part2.R
@@ -1,21 +1,21 @@
 solve_day6_part2 <- function(input) {
   worksheet <- strsplit(input, "")
+  nrows <- length(worksheet)
 
-  nums <- worksheet[1:(length(worksheet)-1)]
+  nums <- worksheet[1:(nrows-1)]
   nums <- as.data.frame(nums)
 
-  ops_row <- worksheet[[length(worksheet)]]
+  ops_row <- worksheet[[nrows]]
   starts <- which(ops_row != " ")
   ends <- c(starts[-1] - 2, NROW(nums))
 
   nums <- as.numeric(apply(nums, 1, paste0, collapse = ""))
-  nums <- Map(\(start, end) nums[start:end], starts, ends)
+  groups <- Map(\(start, end) nums[start:end], starts, ends)
 
   ops <- ops_row[starts]
   ops <- ifelse(ops == "+", list(sum), list(prod))
 
-  ans <- Map(\(fn, vec) fn(vec), ops, nums)
-  ans <- unlist(ans)
+  ans <- mapply(\(fn, vec) fn(vec), ops, groups)
 
   sum(ans)
 }
