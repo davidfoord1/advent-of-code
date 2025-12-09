03c6b31e5df4f98ce1b382659cdc45c87f377921
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-09 11:23:39 +0000

    Add 2025 day 9 part 1 R

---
+++ 2025/R/solve_day9_part1.R
@@ -0,0 +1,13 @@
+solve_day9_part1 <- function(input) {
+  coords <- read.table(text = input, sep = ",", col.names = c("row", "col"))
+  coords <- as.matrix(coords)
+
+  n_corners <- NROW(coords)
+  pairs <- t(combn(n_corners, 2L))
+
+  row_dist <- abs(coords[pairs[, 1], "row"] - coords[pairs[, 2], "row"]) + 1
+  col_dist <- abs(coords[pairs[, 1], "col"] - coords[pairs[, 2], "col"]) + 1
+
+  area <- row_dist * col_dist
+  max(area)
+}
