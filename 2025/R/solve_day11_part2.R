f6a49d0488ff957e3226a0e835199ee6db05eb18
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-11 20:03:51 +0000

    Add 2025 day 11 part 2 R
    Memoisation

--- 2025/R/solve_day11_part2.R
+++ 2025/R/solve_day11_part2.R
@@ -1,50 +1,30 @@
 solve_day11_part2 <- function(input) {
-  first_key <- "svr"
-  last_key <- "out"
-
   devices <- stringi::stri_extract_all_regex(input, r"{.+?(?=:)}")
   outputs <- stringi::stri_extract_all_regex(input, r"{(?<= )\w+}")
   n_devices <- length(input)
 
-  # visited <- new.env(parent = emptyenv(), size = n_devices)
-  queue <- vector("list", n_devices)
-  queue_last <- queue_first <- 1L
-  queue[[queue_last]] <- list(device = first_key, dac = FALSE, fft = FALSE)
-
-  path_count <- 0L
-
-  while (queue_first <= length(queue)) {
-    state <- queue[[queue_first]]
-    device <- state$device
-    pos <- which(devices == device)
-
-    if (identical(device, last_key)) {
-      if (state$dac && state$fft) {
-        path_count <- path_count + 1L
-      }
-    } else {
-      if (identical(device, "dac")) {
-        dac <- TRUE
-      } else {
-        dac <- state$dac
-      }
-
-      if (identical(device, "fft")) {
-        fft <- TRUE
-      } else {
-        fft <- state$fft
-      }
-
-      next_devices <- outputs[[pos]]
-      next_states <- lapply(next_devices, \(x) list(device=x, dac=dac, fft=fft))
-
-      next_in_queue <- queue_last + seq_along(next_devices)
-      queue[next_in_queue] <- next_states
-      queue_last <- queue_last + length(next_devices)
-    }
-
-    queue_first <- queue_first + 1L
-  }
-
-  path_count
+  prod(
+    count_paths("svr", "fft"),
+    count_paths("fft", "dac"),
+    count_paths("dac", "out")
+  )
 }
+
+count_paths <- function(first, last, device_list = devices, output_list = outputs) {
+  if (first == last) return(1L)
+  if (first == "out") return(0L)
+
+  pos <- which(device_list == first)
+  next_devices <- output_list[[pos]]
+
+  dfs <- vapply(
+    next_devices,
+    \(next_device) count_paths(next_device, last),
+    integer(1)
+  )
+
+  sum(dfs)
+}
+
+# the golden ticket
+count_paths <- memoise::memoise(count_paths)
