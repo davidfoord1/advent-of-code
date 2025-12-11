da99645eb3d6e215751d13c58b4498befc9f18b1
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-12-11 22:53:48 +0000

    More structure tweaks

--- 2025/R/solve_day11_part2.R
+++ 2025/R/solve_day11_part2.R
@@ -1,25 +1,24 @@
 solve_day11_part2 <- function(input) {
-  devices <- stringi::stri_extract_all_regex(input, r"{.+?(?=:)}")
-  outputs <- stringi::stri_extract_all_regex(input, r"{(?<= )\w+}")
+  lookup <- stringi::stri_extract_all_regex(input, r"{(?<= )\w+}")
+  names(lookup) <- stringi::stri_extract_all_regex(input, r"{.+?(?=:)}")
   n_devices <- length(input)
 
   prod(
-    count_paths("svr", "fft", devices, outputs),
-    count_paths("fft", "dac", devices, outputs),
-    count_paths("dac", "out", devices, outputs)
+    count_paths("svr", "fft", lookup),
+    count_paths("fft", "dac", lookup),
+    count_paths("dac", "out", lookup)
   )
 }
 
-count_paths <- function(from, to, devices, outputs) {
+count_paths <- function(from, to, lookup) {
   if (from == to) return(1L)
   if (from == "out") return(0L)
 
-  pos <- which(devices == from)
-  next_devices <- outputs[[pos]]
+  next_nodes <- lookup[[from]]
 
   dfs <- vapply(
-    next_devices,
-    \(next_device) count_paths(next_device, to, devices, outputs),
+    next_nodes,
+    \(node) count_paths(node, to, lookup),
     integer(1)
   )
 
